658db1e5df83f5b0b092baf3c907c7aa32c503fe
Brinton <brintonfoyreed@gmail.com>
2021-05-14 09:12:09 -0700

    commit tree 7a894c1d69d674b802005a20ac78975228355b62

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,36 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director, index) =>
+        <div key={index}> 
+        <h3>Name:{director.name}</h3>
+        <ul>
+          {director.movies.map((movie, index) =>  //must take the singular target! Directors => director
+          <li key={index}>{movie}</li>)}
+        </ul>
+        </div>
+      )}
     </div>
-  );
-}
+      )}
+
 
 export default Directors
+
+
+// <div>
+// <h1>Movies Page</h1>
+// {movies.map((movie, index) =>
+//   <div key={index}>
+//     <h3>Name: {movie.title}</h3>
+//     <p>Time: {movie.time}</p>
+//     <p>Genres:</p>
+//     <ul>
+//       {movie.genres.map((genre, index) =>
+//       <li key={index}>{genre}</li>)}
+//     </ul>
+//   </div>
+//   )}
+// </div>
+// );
+// };
